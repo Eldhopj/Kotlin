0addd7ae7454b2f8c26f4e4881e1f267a83ee7ca
Eldhopj <eldho@live.in>
2017-12-23 16:33:31 +0530

    If inside if
    if(cond)
    {
      if(cond)
      {
    -------stmt------
     }
    }

---
+++ NestedIf.kt
@@ -0,0 +1,18 @@
+fun main(args:Array<String>) {
+    var mark: Int?
+    print("Enter you mark")
+    mark = readLine()!!.toInt()
+    if (mark <= 100)
+    {
+        if (mark >= 80 && mark <= 100)
+            print("Grade A")
+        else if (mark >= 60 && mark < 80)
+            print("Grade B")
+        else if (mark >= 40 && mark < 60)
+            print("Grade C")
+        else
+            print("Needs improvement")
+    }
+    else
+        print("Maximum mark is 100")
+}
